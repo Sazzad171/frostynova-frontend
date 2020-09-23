f5881c6b064627a81cb800dd03de5a276976b285
Sazzad171 <sajjad.cse.diu@gmail.com>
2020-09-23 14:53:19 +0600

    service pages added

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,66 +1,3 @@
-$('.owl-carousel').owlCarousel({
-	loop:true,
-	margin:0,
-items:1,
-navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-	nav:true,
-dots:false,
-	responsive:{
-			0:{
-					items:1
-			},
-			767:{
-					items:3
-			},
-			992:{
-					items:5
-			}
-	}
-})
-
-
-/* magnificPopup img view */
-$('.popup-image').magnificPopup({
-type: 'image',
-gallery: {
-	enabled: true
-}
-});
-
-/* magnificPopup video view */
-$('.popup-video').magnificPopup({
-type: 'iframe'
-});
-
-
-
-$('.grid').imagesLoaded( function() {
-// init Isotope
-var $grid = $('.grid').isotope({
-	itemSelector: '.grid-item',
-	percentPosition: true,
-	masonry: {
-	// use outer width of grid-sizer for columnWidth
-	columnWidth: '.grid-item',
-	}
-});
-});
-
-// filter items on button click
-$('.portfolio-menu').on( 'click', 'button', function() {
-var filterValue = $(this).attr('data-filter');
-$grid.isotope({ filter: filterValue });
-});	
-
-
-
-//for menu active class
-$('.portfolio-menu button').on('click', function(event) {
-$(this).siblings('.active').removeClass('active');
-$(this).addClass('active');
-event.preventDefault();
-});
-
 // navbar dropdown
 $('li.dropdown').hover(function() {
 $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(100);
@@ -97,7 +34,7 @@ $('.responsive').slick({
 	dots: false,
 	infinite: true,
 	speed: 300,
-	slidesToShow: 5,
+	slidesToShow: 4,
 	slidesToScroll: 1,
 	autoplay: true,
 	autoplaySpeed: 2000,
@@ -112,18 +49,18 @@ $('.responsive').slick({
 			}
 		},
 		{
-			breakpoint: 600,
+			breakpoint: 667,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1
 			}
 		},
 		{
-			breakpoint: 480,
+			breakpoint: 400,
 			settings: {
-				slidesToShow: 2,
+				slidesToShow: 1,
 				slidesToScroll: 1
 			}
-		}
+		},
 	]
 });
